7e8728765fdcd6f4e1061504d1dc0609aa8b330d
Enoch Hsiao <enochhsiao@hotmail.com>
2021-12-04 01:14:28 -0500

    added geocoder source and destination input, imported axios for api requests

---
+++ elena-frontend/src/components/user-input/source-input.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import get from '../../get';
+import { GEOCODER_API } from '../../Config';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export default function SourceInput({source, setSource}) {
+  const [geocoderResponse, setGeocoderResponse] = useState({
+      data: null,
+      loading: true,
+      error: null,
+  });
+  const [loadingText, setLoadingText] = useState('Press enter to search.')
+  const handleInputChange = (event) => {
+    // user must press enter to send API request
+    setGeocoderResponse({
+      data: null,
+      loading: true,
+      error: null,
+    })
+    setLoadingText("Press enter to search.");
+  }
+
+  const handleEnter = (event) => {
+    const onError = () => {
+
+    }
+    const onSuccess = (resp) => {
+
+    }
+    
+    if (event.target.value.trim() !== '') {
+      setLoadingText("Loading results");
+      const query = `q=${event.target.value}&format=jsonv2`
+      get(setGeocoderResponse, GEOCODER_API + query, onError, onSuccess)
+    }
+  };
+
+  const handleUserSelect= (event, value, reason) => {
+    console.log(value)
+    setSource(value);
+  };
+
+  return (
+    <Box sx={{ marginTop: '30px', width: '80%'  }}>
+      <Typography variant="h4" id="distance-percentage-slider" gutterBottom>
+        Source:
+      </Typography>
+        <Autocomplete
+          id="source-search"
+          disableClearable
+          onChange={handleUserSelect}
+          options={geocoderResponse.data ? geocoderResponse.data : []}
+          loadingText={loadingText}
+          loading={geocoderResponse.loading}
+          noOptionsText="No results found."
+          getOptionLabel={option => option.display_name}
+          filterOptions={(options) => options}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Source"
+              value={source}
+              onChange={handleInputChange}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                    handleEnter(e)
+                }
+              }}
+              InputProps={{
+                ...params.InputProps,
+                type: 'search',
+              }}
+            />
+          )}
+        />
+    </Box>
+  );
+}
--- elena-frontend/src/components/user-input/user-input.js
+++ elena-frontend/src/components/user-input/user-input.js
@@ -3,19 +3,25 @@ import Title from './title.js';
 import ElevationInput from './elevation-input.js';
 import DistanceInput from './distance-input.js';
 import Box from '@mui/material/Box';
+import SourceInput from './source-input';
+import DestinationInput from './destination-input'
 
 export default function UserInput() {
   // "maxElevation" user wants maximum elevation, "minElevation" if user wants minimum elevation for path
   const [elevation, setElevation] = useState('maxElevation');
   // Distance percentage from shortest path
   const [distancePercentage, setDistancePercentage] = useState(25);
-  
+  const [source, setSource] = useState('');
+  const [destination, setDestination] = useState('');
+
   return (
     <div>
       <Title/>
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <ElevationInput elevation={elevation} setElevation={setElevation}/>
         <DistanceInput distancePercentage={distancePercentage} setDistancePercentage={setDistancePercentage}/>
+        <SourceInput source={source} setSource={setSource} />
+        <DestinationInput destination={destination} setDestination={setDestination} />
       </Box>
     </div>
   );
