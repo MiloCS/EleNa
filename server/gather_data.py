0d5697eb47b20716ca8b0335a4e7af5470b1961e
MiloCS <miloccs@gmail.com>
2021-12-06 22:54:28 -0500

    fixing elevation issue

--- server/gather_data.py
+++ server/gather_data.py
@@ -3,21 +3,41 @@ import osmnx
 from collections import OrderedDict
 
 def input_elevations(g):
-    elevation_dict = {}
     outer_param_dict = {}
     inner_param_list = []
     nodes = OrderedDict(g.nodes(data=True))
-
+    max_request_length = 1000
+    
+    #building list of lats and lngs
     for key in nodes:
         nodeval = nodes[key]
         inner_dict = {}
         inner_dict["latitude"] = nodeval['y']
         inner_dict["longitude"] = nodeval['x']
         inner_param_list.append(inner_dict)
-    outer_param_dict['locations'] = inner_param_list
-    outer_param_dict = requests.post('https://api.open-elevation.com/api/v1/lookup', data=outer_param_dict)
 
-    #networkx.set_node_attributes(g, elevation_dict, name='elevation')
+    ipl_len = len(inner_param_list)
+
+    #performing a series of requests to the elevation api
+    results = []
+    curr_index = 0
+    headers_dict = {"Content-Type": "application/json", "Accept": "application/json"}
+    while curr_index < ipl_len:
+        if curr_index+max_request_length < ipl_len:
+            inner_param_list_sect = inner_param_list[curr_index:curr_index+max_request_length]
+        else:
+            inner_param_list_sect = inner_param_list[curr_index:]
+        outer_param_dict["locations"] = inner_param_list_sect
+        result_locations = requests.post('https://api.open-elevation.com/api/v1/lookup', json=outer_param_dict, headers=headers_dict)
+        result_locations = result_locations.json()['results']
+        results.extend(result_locations)
+        curr_index += max_request_length
+    
+    #loading elevation data into format that can be added to graph
+    elevation_dict = {}
+    for i, key in enumerate(nodes.keys()):
+        elevation_dict[key] = results[i]['elevation']
+    networkx.set_node_attributes(g, elevation_dict, name='elevation')
     return g
 
 def load_graph(place_name):
@@ -42,4 +62,8 @@ def get_graph(begin, end, place):
     return g
 
 
+if __name__ == '__main__':
+    mg = pickle.load(open("cache/Amherst.obj", "rb"))
+    mg = input_elevations(mg)
+    print(mg.nodes(data=True))
 
--- server/routing.py
+++ server/routing.py
@@ -1,4 +1,5 @@
 from networkx import single_source_dijkstra as ssd
+from networkx import single_source_bellman_ford as ssbf
 
 class Router:
     def __init__(self, g):
@@ -7,13 +8,21 @@ class Router:
     def get_route(self, start, end, max_length):
         pass
 
+    def elevation_diff(self, nodeNumA, nodeNumB, edge_attrs):
+        nodeA, nodeB = self.g.nodes()[nodeNumA], self.g.nodes()[nodeNumB] 
+        return abs(nodeA['elevation'] - nodeB['elevation'])
+    
+    def neg_elevation_diff(self, nodeA, nodeB, edge_attrs):
+        return -1 * elevation_diff(nodeA, nodeB, edge_attrs)
+
+
 class MinRouter(Router):
     def __init__(self, g, algo="dijkstra"):
         super().__init__(g)
         self.algo = algo
     
     def get_route(self, start, end, max_length):
-        return ssd(self.g, start, end, cutoff=max_length)
+        return ssd(self.g, start, end, weight=self.elevation_diff)
 
 class MaxRouter(Router):
     def __init__(self, g, algo="dijkstra"):
@@ -21,4 +30,4 @@ class MaxRouter(Router):
         self.algo = algo
 
     def get_route(self, start, end, max_length):
-        pass
+        return ssbf(self.g, start, end, weight=self.neg_elevation_diff)
