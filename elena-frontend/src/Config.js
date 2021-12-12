35f63e1a8ff517cdfc1dec658da6ff4969b1ea33
Enoch Hsiao <enochhsiao@hotmail.com>
2021-12-12 16:49:48 -0500

    change to https

--- elena-frontend/src/Config.js
+++ elena-frontend/src/Config.js
@@ -1,9 +1,9 @@
 export const GEOCODER_API = 'https://nominatim.openstreetmap.org/search?'
-export const OVERPASS_API = 'http://overpass-api.de/api/interpreter'
+export const OVERPASS_API = 'https://overpass-api.de/api/interpreter'
 console.log(process.env.REACT_APP_BACKEND)
 export const ROUTING_API = process.env.REACT_APP_BACKEND ? process.env.REACT_APP_BACKEND :'http://localhost:8080/route'
 export function REVERSE_GEOCODER_API(latitude, longitude) {
-  return `http://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1`
+  return `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1`
 }
 export function OVERPASS_REQUEST_BODY(radius, longitude1, latitude1, longitude2, latitude2) {
   return `[out:json];way[highway](around:${radius},${longitude1},${latitude1},${longitude2},${latitude2}); (._;>;);out meta;`
